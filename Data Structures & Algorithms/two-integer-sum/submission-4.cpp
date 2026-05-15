2bebfb8a30d5a1149c16ddcfdbd0ca4155731fe3
Kyle Deliyannides <112675076+kkd16@users.noreply.github.com>
2026-05-14 22:58:10 -0700

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int, int> seen;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+
+            if (seen.contains(need)) {
+                return {seen[need], i};
+            }
+
+            seen[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
