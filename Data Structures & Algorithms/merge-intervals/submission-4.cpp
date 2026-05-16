202db961ee6d11a2abe9955841fad64c256bc101
Kyle Deliyannides <112675076+kkd16@users.noreply.github.com>
2026-05-15 20:43:59 -0700

    Add: merge-intervals - submission-4

---
+++ Data Structures & Algorithms/merge-intervals/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> out;
+
+        out.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            
+            if (intervals[i][0] <= out.back()[1]) {
+
+                out.back()[1] = max(out.back()[1], intervals[i][1]);
+            } else {
+                out.push_back(intervals[i]);
+            }
+        }
+
+        return out;
+        
+    }
+};
