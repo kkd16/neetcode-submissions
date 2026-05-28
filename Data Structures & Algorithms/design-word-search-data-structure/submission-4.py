68dc0a453cddb240f782bb84130ef005e576b484
Kyle Deliyannides <112675076+kkd16@users.noreply.github.com>
2026-05-27 23:32:56 -0700

    Add: design-word-search-data-structure - submission-4

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-4.py
@@ -0,0 +1,44 @@
+class WordDictionary:
+
+    class TrieNode:
+        def __init__(self):
+            self.children = {}
+            self.end = False
+
+    def __init__(self):
+        self.root = self.TrieNode()
+        
+
+    def addWord(self, word: str) -> None: 
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = self.TrieNode()
+            cur = cur.children[c]
+
+        cur.end = True      
+
+    def search(self, word: str) -> bool:
+        
+        def dfs(j, child):
+            cur = child
+
+            for i in range(j, len(word)):
+                c = word[i]
+
+                if c == '.':
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+
+                    cur = cur.children[c]
+
+            return cur.end 
+    
+        return dfs(0, self.root)
+        
