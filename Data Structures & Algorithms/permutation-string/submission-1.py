7dee8eac9e560c85a7a098eeaea34367bbe6e5f8
Kyle Deliyannides <112675076+kkd16@users.noreply.github.com>
2026-06-07 12:47:12 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def idx(self, c: str) -> bool:
+        return ord(c) - ord('a')
+    
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        s1_count = [0] * 26
+
+        for c in s1:
+            s1_count[self.idx(c)] += 1
+
+        n = len(s1)
+
+        s2_count = [0] * 26
+
+        l = 0
+        for r in range(len(s2)):
+
+            s2_count[self.idx(s2[r])] += 1
+
+            if r - l + 1 > n:
+                s2_count[self.idx(s2[l])] -= 1
+                l += 1
+
+            if s1_count == s2_count:
+                return True
+
+
+        return False        
